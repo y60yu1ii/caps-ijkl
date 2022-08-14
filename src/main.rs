9871bc3c907cd3d72ef3f748dce5f72e2b1fdfd3
Dávid Kocsis <kocsis1david@windowslive.com>
2022-08-14 18:20:47 +0200

    simplify waiting for keyboard to appear

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod kbd;
 
 use clap::Clap;
-use std::{thread,process};
 
 #[derive(Clap)]
 #[clap(version = "0.1", author = "Stewart J. Park <hello@stewartjpark.com>")]
@@ -14,32 +13,21 @@ fn main() {
     let opts: Opts = Opts::parse();
     let verbose = opts.verbose;
 
-    // HACK: Keyboard detection thread. If it detects change, die and let systemd restart
-    thread::spawn(|| {
-        kbd::detector::run_forever_until_keyboards_change();
-        println!("Keyboard(s) added/removed. Exitting...");
-
-        // Exit code 2 forces systemd to restart this service. Defined in c-hjkl.service
-        process::exit(2);
-    });
-
-    if let Ok(keyboards) = kbd::enumerator::enumerate_keyboards() {
-        let threads = keyboards
-            .into_iter()
-            .map(|kbd| {
-                println!("Keyboard \"{}\" detected.", kbd.name);
-
-                let mut handler = kbd::handler::KeyboardHandler::new(&kbd.device_path, verbose);
-                thread::spawn(move || {
-                    handler.run_forever();
-                })
-            })
-            .collect::<Vec<_>>();
-
-        for th in threads.into_iter() {
-            th.join().unwrap();
+    let kbd = loop {
+        if let Ok(keyboards) = kbd::enumerator::enumerate_keyboards() {
+            println!("Keyboard count: {}", keyboards.len());
+            if keyboards.len() > 0 {
+                break keyboards.into_iter().next().unwrap();
+            }
+        } else {
+            panic!("Keyboards cannot be detected.");
         }
-    } else {
-        panic!("Keyboards cannot be detected.");
-    }
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    };
+
+    println!("Keyboard \"{}\" detected.", kbd.name);
+
+    let mut handler = kbd::handler::KeyboardHandler::new(&kbd.device_path, verbose);
+    handler.run_forever();
 }
